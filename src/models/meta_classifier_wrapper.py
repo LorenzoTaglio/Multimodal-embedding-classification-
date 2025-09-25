ff3436266ae4e6c45edcc9ad48a27c40c9425531
LorenzoTaglio <lorenzotaglio.work@gmail.com>
2025-09-25 16:40:28 +0200

    add minor changes

--- src/models/meta_classifier_wrapper.py
+++ src/models/meta_classifier_wrapper.py
@@ -3,18 +3,15 @@ import time
 import numpy as np
 
 class MetaClassifierWrapper:
-    def __init__(self, X_train, X_test, y):
+    def __init__(self):
         self.classifier = LogisticRegression(max_iter=1000)
-        self.X_train = X_train
-        self.X_test = X_test
-        self.y = y
 
-    def train_test_base(self):
+    def train_test_base(self, X_train, y_train, X_test):
         time_start = time.time()
         print("\tTraining meta classifier...")
-        self.classifier.fit(self.X_train, self.y)    
+        self.classifier.fit(X_train, y_train)    
         print("\tPredicting...")
-        y_pred_proba = self.classifier.predict_proba(self.X_test)
+        y_pred_proba = self.classifier.predict_proba(X_test)
         y_pred = np.argmax(y_pred_proba, axis=1)
         elapsed = time.time() - time_start
         print(f"Time taken: {elapsed:.2f}")
