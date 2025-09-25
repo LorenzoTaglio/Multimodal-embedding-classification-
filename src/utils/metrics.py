21f06625f0d450f1b41f8484ee66ff1b788a47df
LorenzoTaglio <lorenzotaglio.work@gmail.com>
2025-09-25 16:38:56 +0200

    add accuracy and overral mean"

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 import numpy as np
 import pandas as pd
 
@@ -16,6 +16,7 @@ class metrics:
         self.recall = []
         self.f1 = []
         self.support = []
+        self.accuracy = []
          
 
     def compute(self, y_true, y_pred):
@@ -23,17 +24,21 @@ class metrics:
             y_true, y_pred, average=None, labels=self.labels
         )
         
+        acc = accuracy_score(y_true, y_pred)
+        
         self.precision.append(prec)
         self.recall.append(rec)
         self.f1.append(f1)
         self.support.append(support)
+        self.accuracy.append(acc)
+        
     
     def get_label(label):
         return cui_name_df.loc[label]
         
     
     def to_dataframe(self):
-        return pd.DataFrame({
+        dataframe = pd.DataFrame({
             "class": self.labels,
             "class_name": np.array(get_cui_name(label) for label in self.labels),
             "precision_mean": np.mean(self.precision, axis=0),
@@ -43,5 +48,29 @@ class metrics:
             "f1_mean": np.mean(self.f1, axis=0),
             "f1_std": np.std(self.f1, axis=0),
             "support_mean": np.mean(self.support, axis=0),  # utile per contare i campioni mediamente visti
+            "accuracy_mean": np.mean(self.accuracy),
+            "accuracy_std": np.std(self.accuracy)
         })
+        
+        dataframe.loc[1:, ["accuracy_mean", "accuracy_std"]] = None
+        overall = pd.DataFrame({
+            "class": ["OVERRALL"],
+            "class_name": [None],
+            "precision_mean": [np.mean(dataframe["precision_mean"])],
+            "precision_std": [np.mean(dataframe["precision_std"])],
+            "recall_mean": [np.mean(dataframe["recall_mean"])],
+            "recall_std": [np.mean(dataframe["recall_std"])],
+            "f1_mean": [np.mean(dataframe["f1_mean"])],
+            "f1_std": [np.mean(dataframe["f1_std"])],
+            "support_mean": [np.mean(dataframe["support_mean"])],
+            "accuracy_mean": [np.mean(self.accuracy)],
+            "accuracy_std": [np.std(self.accuracy)],
+        })
+        
+        
+        return pd.concat([dataframe, overall], ignore_index=True)
+        
+    @staticmethod
+    def get_accuracy(y_pred,y_true):
+        return accuracy_score(y_true, y_pred)
         
